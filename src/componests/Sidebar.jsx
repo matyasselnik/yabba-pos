5a46d1111e63cb90523d03503d578f5e8416e6eb
Matyáš Selník <selnikmatyas3@gmail.com>
2026-08-19 13:36:42 +0200

    return cash

--- src/componests/Sidebar.jsx
+++ src/componests/Sidebar.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import "./Sidebar.css"
 import { OrderItem } from "./OrderItem"
 
 export function Sidebar({ items = [], onRemoveItem, onClearOrder }) {
+  const [cashGiven, setCashGiven] = useState("");
 
   const totalSum = items.reduce((sum, item) => sum + item.price * item.count, 0);
+
+  const numericCash = Number(cashGiven) || 0;
+  const change = numericCash > totalSum ? numericCash - totalSum : 0;
 
   return (
     <div id="sidebar-body">
@@ -23,8 +28,9 @@ export function Sidebar({ items = [], onRemoveItem, onClearOrder }) {
 
       <div className="bot">
         <p className="sum">Celkem: <span className="price">{totalSum} Kč</span></p>
+        <p className="return">K vrácení: <span className="return-price">{change} Kč</span></p>
         <div className="btn-row">
-          <p className="cash-btn">Hotovost</p>
+          <input className="cash-btn" type="number" placeholder="Hotovost" value={cashGiven} onChange={(e) => setCashGiven(e.target.value)} />
           <p className="QR-btn">QR platba</p>
         </div>
         <p className="done-btn" onClick={onClearOrder}>Zaplaceno</p>
